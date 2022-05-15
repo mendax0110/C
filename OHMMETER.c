55994f19a8e8a96ceda2f9b06e6c6a4a0b670484
Adrian <52537419+mendax0110@users.noreply.github.com>
2022-05-15 12:41:46 +0200

    Create OHMMETER.c

---
+++ OHMMETER.c
@@ -0,0 +1,37 @@
+/*This is a simple Ohmmeter*/
+/*define the pins and the values*/
+int analogPin       = 0;
+int raw             = 0;
+int Vin             = 5;
+float   Vout        = 0;
+float   R1          = 2000;
+float   R2          = 0;
+float   buffer      = 0;
+
+/*setup the serial communication*/
+void setup()
+{
+    Serial.begin(9600);
+}
+
+/*now do the math and print the serial output*/
+void loop()
+{
+    raw = analogRead(analogPin);
+
+    if(raw)
+    {
+        buffer  = raw * Vin;
+        Vout    = (buffer) / 1024.0;
+        buffer  = (Vin/Vout) - 1;
+        R2      = R1 * buffer;
+
+        Serial.print("Vout: ");
+        Serial.println(Vout);
+        Serial.print("R2: ");
+        Serial.println(R2);
+
+        /*now do the delay*/
+        delay(1000);
+    }
+}
