ddcc7b19a3de49223e201c1b81b78812b966d57b
m3ndax <adrian.goessl@outlook.com>
2022-10-27 21:15:25 +0200

    update code and comment

--- random/itoa.c
+++ random/itoa.c
@@ -1,61 +1,66 @@
+// include the header file
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
 
-void reverse (char s[])
-
+// function to reverse the string
+void reverse(char s[])
 {
-	int		c, i, j;
-	
-	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
-		
-			c = s[i];
-			s[i] = s[j];
-			s[j] = c;
-			
+	int c, i, j;
+
+	for(i = 0, j = strlen(s)-1; i < j; i++, j--)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
 	}
 }
 
-void itoa (int n, char s[])
-
+// function to convert integer to string
+void itoa(int n, char s[])
 {
-	int		i, sign;
-	
+	int i, sign;
+
 	i = 0;
-	if ((sign = n) < 0) {
-		
-		if (n == INT_MIN) {
-			
-				n = -(n + 1);
-				s[i++] = n % 10 + 1 + '0';
-				n /= 10;
-				
-		} else
-		
-				n = -n;
+
+	if((sign = N) < 0)
+	{
+		if(n == INT_MIN)
+		{
+			n = -(n + 1);
+			s[i++] = n % 10 + 1 + '0';
+			n /= 10;
+		}
+		else
+		{
+			n = -n;
+		}
 	}
 	do
-	
-		 s[i++] = n % 10 + '0';
-		 
-	while ((n /= 10) > 0);
-	if (sign < 0)
-	
+	{
+		s[i++] = n % 10 + '0';
+	} 
+	while((n /= 10) > 0);
+	{
+		if(sign < 0)
+		{
 			s[i++] = '-';
-			
-	s[i] = '\0';
-	reverse(s);
+		}
+		s[i] = '\0';
+		reverse(s);
+	}
 }
 
-#define BUFFER_SIZE		16
+// define BUFFER_SIZE
+#define BUFFER_SIZE 16
 
+// main function
 int main()
-
 {
-	char	s[BUFFER_SIZE];
-	
+	char s[BUFFER_SIZE];
+
 	itoa(INT_MIN, s);
 	printf("INT_MIN = %s (%d)\n", s, INT_MIN);
-	
+
 	return 0;
 }
