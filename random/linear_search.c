a5c00b4b81fe00d4180fb90eb8d804c00dd06d87
m3ndax <adrian.goessl@outlook.com>
2022-10-28 21:00:10 +0200

    update comment

--- random/linear_search.c
+++ random/linear_search.c
@@ -1,11 +1,11 @@
-/*include the header files*/
+/*include the header file*/
 #include <stdio.h>
 
 /*main function*/
 int main()
 {
     int num[] = {10, 2, 15, 20, 34, 56, 86};
-    int value, pos = -1, i;
+    int value, pos -1, i;
 
     /*get the input from the user*/
     printf("Enter the value to search: ");
@@ -24,10 +24,12 @@ int main()
     /*print the result*/
     if(pos == -1)
     {
+        /*if the value is not found*/
         printf("Value not found in the array");
     }
     else
     {
+        /*if the value is found*/
         printf("Value found at position %d", pos);
     }
 }
