7a7c356f9c5c718474c85c383ba114fbbbe1739d
m3ndax <adrian.goessl@outlook.com>
2022-10-29 17:42:26 +0200

    update code and comment

--- raspi_pico/pico_tab_led.c
+++ raspi_pico/pico_tab_led.c
@@ -1,26 +1,28 @@
 /*SPDX-License-Identifier: BSD-3-Clause*/
 #include "pico/stdlib.h"
 
-/*this should be a regular LED-Blinking example.
- *resetting the board twice quickly will enter the USB bootloader.
+/*This should be a regular LED-Blinking example.
+ *Resetting the Board twice quickly will enter the USB Bootloader.
  */
-int main()
-{
+
+ int main()
+ {
     #ifdef PICO_DEFAULT_LED_PIN
         const uint LED_PIN = PICO_DEFAULT_LED_PIN;
         gpio_init(LED_PIN);
         gpio_set_dir(LED_PIN, GPIO_OUT);
-        while(true)
+
+        while (true)
         {
             gpio_put(LED_PIN, 1);
-            sleep_ms(250);
+            sleep_ms(500);
             gpio_put(LED_PIN, 0);
-            sleep_ms(250);
+            sleep_ms(500);
         }
     #else
         while(true)
         {
-            sleep_ms(250);
+            sleep_ms(1000);
         }
     #endif
-}
+ }
