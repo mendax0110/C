16ec6bafc3891d79bc49a93bf6f95b42a421db77
m3ndax <adrian.goessl@outlook.com>
2022-11-01 12:20:36 +0100

    update code and comment

--- random/NANO_slowPWM.c
+++ random/NANO_slowPWM.c
@@ -1,38 +1,42 @@
 /*include the librarys*/
 #include <SlowPWM.h>
 
-/*slow PWM with changed frequency depending on inputPWM*/
-int inputPWM;
-SlowPWM myLedPWM(1000, &inputPWM, 13); /*creates a SlowPWM object*/
+/*slow PWM with changed frequency depending on the input value*/
+int intputPWM;
+/*create a new slow PWM object*/
+SlowPWM myLedPWM(1000, %inputPWM, 13);
 
-/*previous time and interval in which inputPWM can change*/
+/*previous time and interval in which inputPWM changes*/
 unsigned long previousMillis = 0;
-const long interval = 10000;
+const long interval = 1000;
 
+/*setup function*/
 void setup()
 {
-    inputPWM = 255;
-    pinMode(13,OUTPUT);
+    intputPWM = 255;
+    pinMode(13, OUTPUT);
 }
 
+/*loop function*/
 void loop()
 {
     myLedPWM.on();
 
-    /*now do smth with inputPWM*/
+    /*change the inputPWM value every interval*/
     unsigned long currentMillis = millis();
+
     if(currentMillis - previousMillis >= interval)
     {
-        /*save the last time yout blinked the LED*/
+        /*save the last time you changed the inputPWM value*/
         previousMillis = currentMillis;
 
-        inputPWM -= 100;
+        inputPWM -= 10;
+
         if(inputPWM < 0)
         {
             inputPWM = 255;
         }
-
+        
         myLedPWM.update();
     }
 }
-
