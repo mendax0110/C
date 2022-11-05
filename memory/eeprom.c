19a7ead4c40d1dc5a01a555ba3c56b7282fdf3fa
m3ndax <adrian.goessl@outlook.com>
2022-11-05 21:00:42 +0100

    update code and comment

--- memory/eeprom.c
+++ memory/eeprom.c
@@ -1,33 +1,37 @@
+/*include the header files*/
 #include <stdio.h>
 #include <avr/io.h>
 #include <avr/eeprom.h>
-
 #include "serial.h"
 
-/*write and read form and to the eeprom*/
-int main(void)
+/*write and read from and to the eeprom*/
+int main()
 {
     initalizeSerial();
     char buf[32];
 
     /*send the data to the eeprom*/
     char byteOfData = 0x12;
+    /*print the data to the serial monitor*/
     sprintf(buf, "initialized: %d\r\n", byteOfData);
     serialWriteString(buf);
 
     /*read the data from the eeprom*/
     unsigned char* eepromAddress = 0x00;
-    byteofData = eeprom_read_byte(eepromAddress);
+    byteOfData = eeprom_read_byte(eepromAddress);
 
+    /*print the data to the serial monitor*/
     sprintf(buf, "after read: %d\r\n", byteOfData);
     serialWriteString(buf);
 
-    /*RW*/
+    /*write the data to the eeprom*/
     byteOfData++;
-    eeprom_write_byte(eepromAddress, byteofData);
+    eeprom_write_byte(eepromAddress, byteOfData);
     byteOfData = eeprom_read_byte(eepromAddress);
 
+    /*print the data to the serial monitor*/
     sprintf(buf, "after write: %d\r\n", byteOfData);
     serialWriteString(buf);
-}
 
+    return 0;
+}
