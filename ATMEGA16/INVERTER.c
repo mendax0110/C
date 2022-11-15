7a6e3f4c34ee3b7d040856ff10f58906537436a8
m3ndax <adrian.goessl@outlook.com>
2022-11-15 21:15:46 +0100

    update code and comment

---
+++ ATMEGA16/INVERTER.c
@@ -0,0 +1,37 @@
+/*
+ * XNOR.c
+ * Author: Adrian
+ */
+
+ /*define the quarz frequency*/
+#define F_CPU 12000000UL
+
+/*include the header files*/
+#include <avr/io.h>
+#include <util/delay.h>
+
+/*inverter  in c ATMEGA16*/
+int main(void)
+{
+    /*set the port as output*/
+    DDRB = 0xFF;
+    /*set the port as input*/
+    DDRC = 0x00;
+    /*set the port as pull up*/
+    PORTC = 0xFF;
+    while (1) 
+    {
+        /*read the input*/
+        if(PINC & (1<<PC0))
+        {
+            /*set the output*/
+            PORTB = 0xFF;
+        }
+        else
+        {
+            /*clear the output*/
+            PORTB = 0x00;
+        }
+    }
+}
+
