1b72a29eb9154e65597c97c0386495190be163f4
m3ndax <adrian.goessl@outlook.com>
2022-11-30 19:43:23 +0100

    update code and comment

--- random/pointer.c
+++ random/pointer.c
@@ -1,23 +1,29 @@
 /*include the header file*/
 #include <stdio.h>
 
-/*main function*/
+/*main function of the program*/
 int main()
 {
     int var;
     int *ptr;
     int **pptr;
 
+    /*actual variable declaration*/
     var = 3000;
+
     /*take the address of var*/
     ptr = &var;
 
-    /*take the addres of ptr using address of operator*/
+    /*take the address of ptr using address of operator &*/
     pptr = &ptr;
 
-    /*take the value using pptr*/
+    /*print var*/
     printf("Value of var = %d\n", var);
+
+    /*print the address of var*/
     printf("Value available at *ptr = %d\n", *ptr);
+
+    /*print the address of ptr*/
     printf("Value available at **pptr = %d\n", **pptr);
 
     return 0;
